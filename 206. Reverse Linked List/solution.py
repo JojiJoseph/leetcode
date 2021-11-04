3123e2c7e1c6f293002d4cd1d6bba2725e543198
joji <jojinattar@gmail.com>
2021-11-04 17:55:15 +0530

    Add reverse linked list

---
+++ 206. Reverse Linked List/solution.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        node = head
+        while node:
+            node.next, prev, node = prev, node, node.next
+        return prev
