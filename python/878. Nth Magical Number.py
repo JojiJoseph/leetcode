3ddf573946eb92df8319726024696860c82fe3df
joji <jojinattar@gmail.com>
2021-12-11 18:53:16 +0530

    Add nth magical number

---
+++ python/878. Nth Magical Number.py
@@ -0,0 +1,20 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b%a, a)
+def lcm(a,b):
+    return a*b/gcd(a,b)
+class Solution:
+    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
+        MOD = int(10**9+7)
+        lower, upper = 0, n*max(a,b)
+        ab = lcm(a, b)
+        while lower <= upper:
+            mid = (lower + upper) // 2
+            candidate = mid//a + mid // b - mid// ab
+            if candidate == n:
+                return (mid - min(mid%a, mid%b, mid%(ab))) % MOD
+            if candidate < n:
+                lower = mid + 1
+            else:
+                upper = mid-1
