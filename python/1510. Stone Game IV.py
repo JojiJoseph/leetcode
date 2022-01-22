9ddfef2f5b09e0de6679600aca9ef4739a6bd49f
joji <jojinattar@gmail.com>
2022-01-22 10:19:31 +0530

    Add stone game IV

---
+++ python/1510. Stone Game IV.py
@@ -0,0 +1,10 @@
+class Solution:
+    @lru_cache(200000)
+    def winnerSquareGame(self, n: int) -> bool:
+        if int(sqrt(n))**2 == n:
+            return True
+        lim = int(sqrt(n)) + 1
+        win = False
+        for i in range(1, lim):
+            win = win or not self.winnerSquareGame(n-i*i)
+        return win
